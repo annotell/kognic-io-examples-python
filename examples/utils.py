ec22f44ced67ed5685715d7da7f54085061d8af8
annotell <github-actions@github.com>
2025-05-27 08:25:14 +0000

    Merge pull request #156 from annotell/improve-tests
    Improve tests

--- examples/utils.py
+++ examples/utils.py
@@ -4,14 +4,28 @@ from kognic.io.client import KognicIOClient
 from kognic.io.model import SceneStatus
 
 
-def wait_for_scene_job(client: KognicIOClient, scene_uuid: str, timeout=20) -> SceneStatus:
+class SceneException(Exception):
+    pass
+
+
+class SceneFailedException(SceneException):
+    pass
+
+
+class SceneNotFinishedException(SceneException):
+    pass
+
+
+def wait_for_scene_job(client: KognicIOClient, scene_uuid: str, timeout=20, fail_on_failed: bool = True) -> SceneStatus:
     start_time = time.time()
     while (time.time() - start_time) < timeout:
         response = client.scene.get_scenes_by_uuids(scene_uuids=[scene_uuid])
         scene = response[0]
+        if scene.status == SceneStatus.Failed and fail_on_failed:
+            raise SceneFailedException(f"Scene creation failed: {scene.error_message}")
         if scene.status in [SceneStatus.Created, SceneStatus.Failed]:
             return scene.status
 
         time.sleep(1)
 
-    raise Exception(f"Job was not finished: {scene_uuid}")
+    raise SceneNotFinishedException(f"Job was not finished: {scene_uuid}")
